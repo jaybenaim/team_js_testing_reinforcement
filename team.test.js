9383b6cc68fe2f8e379305fd7de5a9b37763fccd
jaybenaim <benaimjacob@gmail.com>
2019-09-01 21:10:03 -0400

    complete

--- team.test.js
+++ team.test.js
@@ -1,5 +1,7 @@
-const teamObj = require('./team.js');
+const teamObj = require("./team.js");
 
-// test('', () => {
-//
-// });
+test("should create an object when the team name and level being passed", () => {
+  let obj = { teamName: "jay", level: 2, points: 0 };
+  let assert = teamObj("jay", 2);
+  expect(assert).toEqual(obj);
+});
